209e3efd33e140fb0946506656fbc8b9d49876d8
Dave Gumba <dave_gumba@Yahoo.com>
2017-07-05 16:58:54 -0400

    modified

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ myName.onmouseover = function() {
   let randColor = colors[randIndex];
   this.style.color = randColor;
 }
+
+/*
+var statusDescription = document.getElementById('status-description');
+var d = new Date();
+
+var currentHour = d.getHours();
+
+if (currentHour >= 6 && currentHour <= 17) {
+  statusDescription.innerHTML = 'Currently training in the dungeon';
+} else if (currentHour > 17 && currentHour <= 18) {
+  statusDescription.innerHTML = 'Eating a big, nutritious meal.';
+} else if (currentHour > 18 && currentHour < 20) {
+  statusDescription.innerHTML = 'Healing for the next day.';
+} else if (currentHour <= 6 && currentHour >= 20) {
+  statusDescription.innerHTML = 'Sleeping. zzz';
+}
+*/
